ee274e3908eb5cc05de4dab1b56823d7cdae9148
Local Acount <Local Acount@10.198.75.52>
2021-05-07 20:01:49 -0300

    this is second coment

--- src/MainATM1.java
+++ src/MainATM1.java
@@ -1,4 +1,9 @@
 
 public class MainATM1 {
-
+	
+	public static void main(String args[]){
+System.out.println("welcome here");
+System.out.println("welcome here2");
+System.out.println("welcome here3");
+}
 }
